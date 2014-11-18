c8bc3cc9ff00ee902928f733806f662192d655e4
Gabriel Piche <gabriel.piche@hec.ca>
2014-11-18 15:58:50 -0500

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -17,7 +17,9 @@ NEI_R <- filter(NEI_R, (NEI_R$fips == "24510" | NEI_R$fips == "06037") & NEI_R$t
 Question6 <- aggregate(Emissions ~ year + fips, NEI_R, sum)
 
 #Plotting the chart
-png(file="plot6.png")
+dev.copy(png, file="plot6.png")
+
 plot6_R <- qplot(year, Emissions, data = Question6, color = fips, geom="line", xlab = "Year", ylab = "Emissions of" ~ PM[2.5] ~ "(tons)", main = "Emissions from motor vehicle sources")
 plot6 <- plot6_R +theme(legend.title = element_text(colour="black", size=10))+ theme(plot.title = element_text(size=14, face="bold")) + scale_colour_hue(name = "County",labels=c("Los Angeles County","Baltimore City"))
+
 dev.off()
